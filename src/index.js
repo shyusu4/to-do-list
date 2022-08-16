e001adedb913a16d1e0070cafe2b8cc17582f6b7
unknown <shyusupova4@gmail.com>
2022-08-16 15:26:22 +0500

    Add required changes

--- src/index.js
+++ src/index.js
@@ -1,31 +1,28 @@
 import './style.css';
-import loadTasks from './modules/display';
-import {
-  addTodo
-} from './modules/crud';
-
+import List from './modules/crud';
+ 
 const todoForm = document.querySelector('.todo-form');
 const todoList = document.querySelector('.todo-list');
 const todoInput = document.querySelector('.todo-input');
 const addBtn = document.querySelector('.add-button');
-
+const list = new List();
+list.loadTasks();
+ 
 const load = () => {
   if (todoInput.value.trim()) {
-    addTodo(todoInput.value);
+    list.addTodo(todoInput.value);
     todoList.innerHTML = '';
     todoInput.value = '';
-    loadTasks();
+    list.loadTasks();
   }
 };
-
+ 
 todoForm.addEventListener('submit', (e) => {
   e.preventDefault();
   load();
 });
-
+ 
 addBtn.addEventListener('click', (e) => {
   e.preventDefault();
   load();
 });
-  
-loadTasks();
